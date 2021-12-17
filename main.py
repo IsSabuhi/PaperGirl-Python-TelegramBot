21947735888df2234e9611fb2dfab7307837c6a5
IsSabuhi <sabuhi.israfilov2001@gmail.com>
2021-12-17 18:34:54 +0700

    change main :)

--- main.py
+++ main.py
@@ -1,16 +1,60 @@
-# This is a sample Python script.
+import json
+import logging
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from aiogram.utils.markdown import hlink, hbold
+
+import config
+from aiogram import Bot, Dispatcher, types, executor
+from aiogram.dispatcher.filters import Text
+from parser import parser_discounts_game
+
+# инициализируем бота
+bot = Bot(token=config.API_TOKEN, parse_mode=types.ParseMode.HTML)
+# Диспетчер для бота
+dp = Dispatcher(bot)
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@dp.message_handler(commands="start")
+async def start(message: types.Message):
+    start_buttons = ["Игры со скидкой EG", "Магазин EG", "Информация"]
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True).row()
+    keyboard.add(*start_buttons)
+
+    await message.answer('Привет {0.first_name}'.format(message.from_user), reply_markup=keyboard)
 
 
-# Press the green button in the gutter to run the script.
+@dp.message_handler(Text(equals="Игры со скидкой EG"))
+async def get_discount_Game(message: types.Message):
+    await message.answer("Пожалуйста подождите...")
+
+    parser_discounts_game()
+
+    with open("new_json.json") as file:
+        data = json.load(file)
+
+    for item in data:
+        card = f"{hlink(item.get('url'), item.get('url'))}\n" \
+               f"{hbold('Заголовок: ')} {item.get('title')}\n" \
+               f"{hbold('Описание: ')} {item.get('description')}\n" \
+               f"{hbold('Цена: ')} {item.get('price')}\n" \
+               f"{hbold('Цена со скидкой: ')} {item.get('discountPrice')}\n" \
+               f"{hbold('Дата начала акции: ')} {item.get('startDate')}\n" \
+               f"{hbold('Дата конца акции: ')} {item.get('endDate')}\n" \
+
+        await message.answer(card)
+
+
+@dp.message_handler(Text(equals="Магазин EG"))
+async def ShopEG(message: types.Message):
+    await bot.send_message(message.from_user.id, "Ваша ссылка на магазин Epic games " + str('epicgames.com/'))
+
+
+@dp.message_handler(Text(equals="Информация"))
+async def Info(message: types.Message):
+    await bot.send_message(message.from_user.id, "Это кнопка пока не работает)")
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    executor.start_polling(dp)
